756c113f0f30057c0bf9448e0fd9b707fe94bd2d
Vitolus <vito.davide00@gmail.com>
2023-10-23 22:34:43 +0200

    optimize methods

--- sudoku.py
+++ sudoku.py
@@ -19,27 +19,24 @@ class Sudoku:
         if not self.board:  # if the board is empty
             return False  # board can't be solved
 
-        if all(len(v) == 1 for k, v in self.board.items()):  # if all the cells have only one value
+        if all(len(v) == 1 for v in self.board.values()):  # if all the cells have only one value
             return True  # board is solved
 
-        # get the cell with the least number of possible values
-        length, k = min((len(val), key) for key, val in self.board.items() if len(val) > 1)
-        for num in self.board[k]:  # iterate over all the possible values of the cell
-            new_board = dict(list(self.board.items()))  # create a new board
+        k, values = min((k, v) for k, v in self.board.items() if len(v) > 1)  # get the cell with the least values
+        for num in values:  # iterate over all the values
+            new_board = self.board.copy()  # create a copy of the board
             new_board[k] = num  # assign the value to the cell
-            try_to_solve_new_config = Sudoku(new_board).solve()  # try to solve the new board
-            if try_to_solve_new_config:  # if the new board is solved
+            if Sudoku(new_board).solve():  # if the board is solved
                 return True  # board is solved
 
         return False  # board can't be solved
 
-    def __eliminate(self):  # eliminate the values from the peers
+    def __eliminate(self):  # eliminate the values of solved cells from their peers
         for k, v in self.board.items():  # iterate over all the cells
-            if len(v) != 1:  # if the cell needs elimination
-                peers = self.peer_dict[k]  # get all the peers
-                # get all the values of the peers
-                peer_values = set([self.board[p] for p in peers if len(self.board[p]) == 1]) 
-                self.board[k] = ''.join(set(self.board[k]) - peer_values)  # remove the peer values from the cell
+            if len(v) != 1:  # if the cell has more than one value
+                peers = self.peer_dict[k]  # get the peers of the cell
+                peer_values = {self.board[p] for p in peers if len(self.board[p]) == 1}  # get the values of the peers
+                self.board[k] = ''.join(set(v) - peer_values)  # remove the values of the peers from the cell
 
     def __only_choice(self):  # assign the value to the cell if it's the only choice
         for unit in self.units:  # iterate over all the units
@@ -49,14 +46,10 @@ class Sudoku:
                     self.board[num_places[0]] = num  # assign the value to the cell
 
     def __run_episode(self):  # run the elimination and only choice strategy
-        stuck = False
-        while not stuck:  # while we're not stuck
-            # count the number of solved cells
-            solved_values_before = len([cell for cell in self.board.keys() if len(self.board[cell]) == 1])
-            self.__eliminate()  # elimination strategy
-            self.__only_choice()  # only choice strategy
-            # count the number of solved cells
-            solved_values_after = len([cell for cell in self.board.keys() if len(self.board[cell]) == 1])
-            stuck = solved_values_before == solved_values_after  # we're stuck if we didn't solve any new cells
-            if len([cell for cell in self.board.keys() if len(self.board[cell]) == 0]):  # if any cell has no value
-                return False  # board can't be solved
+        while True:  # run until the board is solved or can't be solved
+            solved_values_before = sum(len(v) == 1 for v in self.board.values())  # get the number of solved cells
+            self.__eliminate()  # eliminate the values of solved cells from their peers
+            self.__only_choice()  # assign the value to the cell if it's the only choice
+            solved_values_after = sum(len(v) == 1 for v in self.board.values())  # get the number of solved cells
+            if solved_values_before == solved_values_after:  # if the board can't be solved
+                return False if any(len(v) == 0 for v in self.board.values()) else True  # return False if any cell has
