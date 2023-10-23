c11495feaf743434db4049e6ecf5ddedf3b21d06
Vitolus <vito.davide00@gmail.com>
2023-10-23 22:52:25 +0200

    update

--- helper.py
+++ helper.py
@@ -1,17 +1,1 @@
-def cartesian_product(x, y):  # cartesian product of x and y
-    return [a + b for a in x for b in y]  # return the cartesian product of x and y
-
-
-ROWS = 'ABCDEFGHI'  # rows of the board
-COLS = '123456789'  # columns of the board
-CELLS = cartesian_product(ROWS, COLS)  # all the cells of the board
-
-
-def display(values):  # display the board
-    print('')  # add some space
-    width = max(len(values[s]) for s in CELLS) + 1  # find the width of the cell with the most values
-    line = '+'.join(['-' * (width * 3)] * 3)  # create a line to separate the boxes
-    for r in ROWS:  # iterate over all the rows
-        print(''.join(values[r + c].center(width) + ('|' if c in '36' else '') for c in COLS))  # print the row
-        if r in 'CF':  # if the row is C or F
-            print(line)  # print the line
+from itertools import product
--- sudoku.py
+++ sudoku.py
@@ -1,18 +1,36 @@
 import helper as h
+from itertools import product
 
 
 class Sudoku:
+    _ROWS = 'ABCDEFGHI'  # rows of the board
+    _COLS = '123456789'  # columns of the board
+    _CELLS = [''.join(cell) for cell in product(_ROWS, _COLS)]  # all the cells of the board
+
     def __init__(self, board):
         self.board = board
-        self.units = ([h.cartesian_product(i, h.COLS) for i in h.ROWS] +  # row units
-                      [h.cartesian_product(h.ROWS, j) for j in h.COLS] +  # column units
+        self.units = ([[''.join(p) for p in product(i, self._COLS)] for i in self._ROWS] +  # row units
+                      [[''.join(p) for p in product(self._ROWS, j)] for j in self._COLS] +  # column units
                       # cell units
-                      [h.cartesian_product(i, j) for i in ['ABC', 'DEF', 'GHI'] for j in ['123', '456', '789']])
+                      [[''.join(p) for p in product(i, j)] for i in ['ABC', 'DEF', 'GHI']
+                       for j in ['123', '456', '789']])
         # map each cell to its peers
-        self.peer_dict = dict((cell,
-                               set(sum(dict((cell,
-                                             [unit for unit in self.units if cell in unit]) for cell in h.CELLS)[cell],
-                                       [])) - {cell}) for cell in h.CELLS)
+        self.peer_dict = dict(
+            (cell, set(sum(dict(
+                (cell, [unit for unit in self.units if cell in unit]) for cell in self._CELLS)[cell], []))
+             - {cell}) for cell in self._CELLS)
+
+    @property
+    def ROWS(self):
+        return self._ROWS
+
+    @property
+    def COLS(self):
+        return self._COLS
+
+    @property
+    def CELLS(self):
+        return self._CELLS
 
     def solve(self):  # solve the board
         self.__run_episode()  # run the elimination and only choice strategy
@@ -40,7 +58,7 @@ class Sudoku:
 
     def __only_choice(self):  # assign the value to the cell if it's the only choice
         for unit in self.units:  # iterate over all the units
-            for num in h.COLS:  # iterate over all the numbers
+            for num in self._COLS:  # iterate over all the numbers
                 num_places = [cell for cell in unit if num in self.board[cell]]  # get all the cells with the number
                 if len(num_places) == 1:  # if the number only appears once in the unit
                     self.board[num_places[0]] = num  # assign the value to the cell
@@ -53,3 +71,12 @@ class Sudoku:
             solved_values_after = sum(len(v) == 1 for v in self.board.values())  # get the number of solved cells
             if solved_values_before == solved_values_after:  # if the board can't be solved
                 return False if any(len(v) == 0 for v in self.board.values()) else True  # return False if any cell has
+
+    def display(self, values):  # display the board
+        width = max(len(values[s]) for s in self._CELLS) + 1  # get the width of the cell
+        line = '+'.join(['-' * (width * 3)] * 3)  # create a line
+        for r in self._ROWS:  # iterate over all the rows
+            print(
+                ''.join(values[r + c].center(width) + ('|' if c in '36' else '') for c in self._COLS))  # print the row
+            if r in 'CF':  # if the row is C or F
+                print(line)  # print the line
