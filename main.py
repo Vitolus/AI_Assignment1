4c678e59d44ca582ed0607343c70336113fd0031
Vitolus <vito.davide00@gmail.com>
2023-10-24 17:13:49 +0200

    change name and add plot of execution time

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sudoku as s
+import sudoku_propagation as sp
 from itertools import product
+import numpy as np
+import matplotlib.pyplot as plt
+import time
 
 START_BOARD = '53..7....6..195....98....6.8...6...34..8.3..17...2...6.6....28....419..5....8..79'
 
@@ -14,3 +17,25 @@ if __name__ == '__main__':
             board[k] = '123456789'  # replace it with all possible values
     sudoku.solve()  # solve the board
     sudoku.display(sudoku.board)  # display the board
+    run = sudoku.exec_time
+    total = []
+    for i in range(100):
+        start_time = time.perf_counter()
+        sp.Sudoku(board).solve()
+        total.append((time.perf_counter() - start_time) * 1000)
+    print(sudoku.exec_time)
+    # plot the time taken to solve the board
+    plt.figure()
+    plt.plot(np.arange(1, len(run) + 1), run, label='time per episode')
+    plt.title('Time taken to solve the board')
+    plt.xlabel('episode iteration')
+    plt.ylabel('time (microseconds)')
+    plt.legend()
+    plt.figure()
+    plt.plot(np.arange(1, len(total) + 1), total, label='time per run')
+    plt.plot(np.arange(1, len(total) + 1), [np.mean(total)] * len(total), label='mean')
+    plt.title('Time taken to solve the board 100 times')
+    plt.xlabel('run iteration')
+    plt.ylabel('time (milliseconds)')
+    plt.legend()
+    plt.show()
--- sudoku_annealing.py
+++ sudoku_annealing.py
@@ -1,4 +1,5 @@
 from itertools import product
+import time
 
 
 class Sudoku:
@@ -7,6 +8,7 @@ class Sudoku:
         self._COLS = '123456789'  # all the columns
         self._CELLS = [''.join(cell) for cell in product(self._ROWS, self._COLS)]  # all the cells
         self.board = board
+        self.exec_time = [] # list to store the time taken to solve the board
         self.units = ([[''.join(p) for p in product(i, self._COLS)] for i in self._ROWS] +  # row units
                       [[''.join(p) for p in product(self._ROWS, j)] for j in self._COLS] +  # column units
                       # cell units
@@ -20,9 +22,6 @@ class Sudoku:
 
     def solve(self):  # solve the board
         self.__run_episode()  # run the elimination and only choice strategy
-        if not self.board:  # if the board is empty
-            return False  # board can't be solved
-
         if all(len(v) == 1 for v in self.board.values()):  # if all the cells have only one value
             return True  # board is solved
 
@@ -32,37 +31,40 @@ class Sudoku:
             new_board[k] = num  # assign the value to the cell
             if Sudoku(new_board).solve():  # if the board is solved
                 return True  # board is solved
-
         return False  # board can't be solved
 
-    def __eliminate(self):  # eliminate the values of solved cells from their peers
+    def __constraint_propagation(self):  # eliminate the values of solved cells from their peers
         for k, v in self.board.items():  # iterate over all the cells
             if len(v) != 1:  # if the cell has more than one value
                 peers = self.peer_dict[k]  # get the peers of the cell
                 peer_values = {self.board[p] for p in peers if len(self.board[p]) == 1}  # get the values of the peers
                 self.board[k] = ''.join(set(v) - peer_values)  # remove the values of the peers from the cell
 
-    def __only_choice(self):  # assign the value to the cell if it's the only choice
+    def __set_value(self):  # assign the value to the cell if it's the only choice
         for unit in self.units:  # iterate over all the units
             for num in self._COLS:  # iterate over all the numbers
                 num_places = [cell for cell in unit if num in self.board[cell]]  # get all the cells with the number
                 if len(num_places) == 1:  # if the number only appears once in the unit
                     self.board[num_places[0]] = num  # assign the value to the cell
 
-    def __run_episode(self):  # run the elimination and only choice strategy
+    def __run_episode(self):  # run the constraint propagation and only choice strategy
         while True:  # run until the board is solved or can't be solved
+            start_time = time.perf_counter()  # get the start time
             solved_values_before = sum(len(v) == 1 for v in self.board.values())  # get the number of solved cells
-            self.__eliminate()  # eliminate the values of solved cells from their peers
-            self.__only_choice()  # assign the value to the cell if it's the only choice
+            self.__constraint_propagation()  # eliminate the values of solved cells from their peers
+            self.__set_value()  # assign the value to the cell if it's the only choice
             solved_values_after = sum(len(v) == 1 for v in self.board.values())  # get the number of solved cells
-            if solved_values_before == solved_values_after:  # if the board can't be solved
-                return False if any(len(v) == 0 for v in self.board.values()) else True  # return False if any cell has
+            # append the time taken to solve the board in microseoconds
+            self.exec_time.append((time.perf_counter() - start_time) * 100000)
+            if solved_values_before == solved_values_after:  # if the number of solved cells hasn't changed
+                # return False if any cell has no value and True otherwise
+                return False if any(len(v) == 0 for v in self.board.values()) else True 
 
     def display(self, values):  # display the board
         width = max(len(values[s]) for s in self._CELLS) + 1  # get the width of the cell
         line = '+'.join(['-' * (width * 3)] * 3)  # create a line
         for r in self._ROWS:  # iterate over all the rows
             print(
-                ''.join(values[r + c].center(width) + ('|' if c in '36' else '') for c in self._COLS))  # print the row
+                ''.join(values[r + c].center(width) + ('|' if c in '36' else '') for c in self._COLS))  # print the row 
             if r in 'CF':  # if the row is C or F
                 print(line)  # print the line
